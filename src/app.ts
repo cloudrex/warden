e0e0c8889a7c020457b7046ae83fc7fa2c9d5621
CloudRex <cloudrex@outlook.com>
2018-09-21 21:25:47 -0400

    Added vscode config folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Mongo from "./database/mongo-database";
 import {GuildMember, Message} from "discord.js";
 import path from "path";
+
 import {
     ArgumentTypeChecker,
     Bot,
@@ -13,6 +14,7 @@ import {
     Utils,
     on
 } from "discord-anvil";
+
 import {ArgumentResolver} from "discord-anvil/dist/commands/command";
 import WardenAPI from "./core/warden-api";
 
