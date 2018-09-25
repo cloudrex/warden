38ec9eb66d49ec3f08580d8dad10d669a09ff509
CloudRex <cloudrex@outlook.com>
2018-09-24 23:04:37 -0400

    Added unsaved missing files

--- src/commands/moderation/case.ts
+++ src/commands/moderation/case.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Command, CommandContext} from "discord-anvil";
 import {CommandType} from "../general/help";
 import {Argument} from "discord-anvil/dist";
@@ -40,46 +39,3 @@ export default class Case extends Command {
         await context.fail("Not yet implemented");
     }
 };
-=======
-import {Command, CommandContext} from "discord-anvil";
-import {CommandType} from "../general/help";
-import {Argument} from "discord-anvil/dist";
-import {RestrictGroup} from "discord-anvil/dist/commands/command";
-import Mongo from "../../database/mongo-database";
-import {GuildMember} from "discord.js";
-import ChatEnvironment from "discord-anvil/dist/core/chat-environment";
-
-type ClearWarnsArgs = {
-    readonly member: GuildMember;
-};
-
-export default class Case extends Command {
-    readonly type = CommandType.Moderation;
-
-    readonly meta = {
-        name: "clearwarns",
-        description: "Clear all warnings from an user"
-    };
-
-    readonly arguments: Array<Argument> = [
-        {
-            name: "member",
-            description: "The target user",
-            type: "member",
-            required: true
-        }
-    ];
-
-    constructor() {
-        super();
-
-        this.restrict.environment = ChatEnvironment.Guild;
-        this.restrict.specific = [RestrictGroup.ServerModerator];
-    }
-
-    public async executed(context: CommandContext, args: ClearWarnsArgs): Promise<void> {
-        // TODO
-        await context.fail("Not yet implemented");
-    }
-};
->>>>>>> ff606c27c5c085893a2dfcb23b7a33c90fd20a01
