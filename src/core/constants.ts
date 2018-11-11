8562234685e482b509e57c1772cded4edc600ea6
CloudRex <cloudrex@outlook.com>
2018-11-11 17:06:55 -0500

    Added constants disclaimer

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,3 +1,6 @@
+// Warning: Words contained in this file might offensive to some, they are here for moderation purposes.
+// If you're not willing to see/read words that may be offensive to you, please close this file immediatly.
+
 export const BadWords: string[] = [
     "asshole",
     "fuck",
